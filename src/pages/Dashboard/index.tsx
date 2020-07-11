19399464974ab187fc422fe8d67c52a9c5b15206
Ivan Martini <ivan.martini@extremedigital.com.br>
2020-07-11 15:45:30 -0300

    styling dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,56 @@
 import React from 'react';
-import { Title } from './style';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './style';
+import logoGit from '../../assets/gitrocket.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Dashboar</Title>;
+  return (
+    <>
+      <img src={logoGit} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="#teste.com.br">
+          <img
+            src="https://avatars3.githubusercontent.com/u/36418410?s=460&u=c3d0fe1af08b8fac6a695dbc3bb47d956063224a&v=4"
+            alt="Ivan Martini"
+          />
+          <div>
+            <strong>ferrarimartini/uniform</strong>
+            <p>Easy peasy gihly scalable ReactJS</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="#teste.com.br">
+          <img
+            src="https://avatars3.githubusercontent.com/u/36418410?s=460&u=c3d0fe1af08b8fac6a695dbc3bb47d956063224a&v=4"
+            alt="Ivan Martini"
+          />
+          <div>
+            <strong>ferrarimartini/uniform</strong>
+            <p>Easy peasy gihly scalable ReactJS</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="#teste.com.br">
+          <img
+            src="https://avatars3.githubusercontent.com/u/36418410?s=460&u=c3d0fe1af08b8fac6a695dbc3bb47d956063224a&v=4"
+            alt="Ivan Martini"
+          />
+          <div>
+            <strong>ferrarimartini/uniform</strong>
+            <p>Easy peasy gihly scalable ReactJS</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
